58c8874dcdd309c9ebad04613230d6fb22c82f30
AKhan-CIR <akhan@codeinreach.com>
2019-09-19 11:30:59 -0700

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ document.addEventListener("DOMContentLoaded", function() {
+ 
+
+ document.querySelector("#create-task-form").addEventListener("submit", function(e){
+ e.preventDefault();
+
+ //Get value from form
+ const todo = document.querySelector("#new-task-description").value;
+ //Create new li
+ const li = document.createElement("li");
+ //Select Location to add List
+ const ul = document.querySelector("#tasks");
+     
+ addListItem(todo, li, ul)
+ //Reset Form Data   
+ e.target.reset()
+ });
 });
+
+function addListItem(todo, li, ul) {
+  //Add id to to li for future access
+  li.setAttribute("id", "tasks-list-item");
+  //Add form value to li
+  li.innerText =  todo;
+  //Add li to UL
+  ul.appendChild(li);
+}
